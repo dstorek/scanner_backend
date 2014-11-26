a1ccc74d38b5aad52cee4c668a18d72ff9c7ff2c
Daniel Storek <dstorek@post.cz>
2014-11-26 22:40:18 +0100

    added voiding

--- scanner_backend.js
+++ scanner_backend.js
@@ -4,18 +4,20 @@ var express = require("express");
 var app = express();
 var morgan = require("morgan");
 var port = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
 
 // define hardcoded data
 var barcodeId = "1";
 var serialNumber = "9";
 var myPass = {
-    isValid: true,
+    voided: false,
     id: "myPass",
     balance: 0
 };
 
 // configure the app
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 // define routes
 app.get('/', function(req,res){
@@ -36,6 +38,28 @@ app.post("/testing/serial/:serialNumber", function(req,res){
 
     if (req.params.serialNumber === serialNumber) {
         res.json({"pass": myPass}).end();
+    } else {
+        res.json({"pass": "no data"}).end();
+    }
+});
+
+app.post("/testing/serial/:serialNumber/update", function(req,res){
+
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    if (req.params.serialNumber === serialNumber) {
+        if (req.body.voided === "true" && myPass.voided === false) {
+            myPass.voided = true;
+            res.json({"pass": myPass}).end();
+        } else if ((req.body.voided === "true" && myPass.voided === true)) {
+            res.send("already voided").end()
+        } else {
+            res.send("no change").end()
+        }
+        res.json({"pass": myPass}).end();
+
     } else {
         res.json({"pass": "no data"}).end();
     }
